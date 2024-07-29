55ed4fc8b90eb4ad864fd4e6f2cd2b2ce9fd5915
howabsurd <poptanikrish1@gmail.com>
2024-07-29 17:45:16 +0530

    Water effect and noisy background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bricolage_Grotesque, Lancelot, Oswald } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import GrainEffect from "@/components/visualEffects/grain-effect";
+import { Cursor } from "@/components/cursor/cursor";
 
-const inter = Inter({ subsets: ["latin"] });
+// Fonts for example
+const MainFont = Bricolage_Grotesque({ subsets: ["latin"] });
+const OswaldFont = Oswald({subsets: ["latin"], variable: "--font-oswald"});
+const LancelotFont = Lancelot({
+  subsets: ["latin"], variable: "--font-lancelot",
+  weight: "400"
+});
 
 export const metadata: Metadata = {
   title: "KRISH POPTANI",
@@ -16,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(MainFont.className, OswaldFont.variable, LancelotFont.variable)}>
+        <GrainEffect />
+        <Cursor color="#fff" />
+        {children}
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
+
 import Image from "next/image";
 import clsx from "clsx";
 import { cn } from "@/lib/utils";
+import { WaterWaveWrapper } from "@/components/visualEffects/water-wave-wrapper";
 export default function Home() {
   return (
-    <div>
-      <h1 className={cn("italic", "text-yellow-400", true && "underline")}>Hello</h1>
-    </div>
+    <WaterWaveWrapper imageUrl="" dropRadius="3" perturbance="3" resolution="2048">
+      {()=> <div className="h-screen"></div>}
+    </WaterWaveWrapper>
   );
 }
