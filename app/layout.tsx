7473294f75925e913c7f0d956fbb781122115df4
howabsurd <poptanikrish1@gmail.com>
2024-07-29 16:22:52 +0530

    Some initial changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import React from "react"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-export const metadata = {
-  title: 'KRISH POPTANI',
-  description: 'Krish Poptani official portfolio',
-}
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "KRISH POPTANI",
+  description: "Krish Poptani official portfolio",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import React from "react";
 
 export default function Home() {
   return (
-    <main className="">
-       Hello my friends
+    <main className="text-xl">
+     Helo Eo
     </main>
   );
 }
